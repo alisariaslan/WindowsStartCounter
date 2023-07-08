202f2cadc9fac4680a0f42de747fdbc0765f084a
Ali <Ali@ALI-DESKTOP>
2023-07-08 09:40:32 +0300

    ready to rock in roll

--- StartCounter.bat
+++ StartCounter.bat
@@ -1,13 +1,25 @@
-
-	
-@ set counterfile=startcounts.txt
-@ echo Start Counter initializing....
-@ if exist %counterfile% (
-	@ echo %counterfile% file found.
+@echo off
+set counterfile=startcounts.txt
+echo Start Counter initializing.... 
+if exist %counterfile% (
+	echo %counterfile% file found. 
 ) else (
-	@ echo %counterfile% file NOT FOUND!
-	@ echo 0 >> %counterfile%
-	@ echo File is created.
+	echo %counterfile% file NOT FOUND!
+	echo 0 > %counterfile%
+	echo File is created. 
 )
-@ set /p counter=<%counterfile%
-@ set /a counter+=1
+set /p counter=<%counterfile%
+set /a counter+=1
+echo %counter% > %counterfile%
+echo ##########################################
+echo This machine has been started total of %counter%
+echo ##########################################
+echo You can close this window if you want.
+set /p "isresetwanted=Do you want to reset the counter? (yes/no) : "
+if %isresetwanted%==yes (
+	echo 0 > %counterfile%
+	echo Done.
+) else (
+	echo Ok.
+)
+pause
